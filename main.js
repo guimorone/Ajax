9d33bb09c57f5262c2a5ba8ff6e237811a5f2294
Guilherme Morone Araujo <guimorone@gmail.com>
2020-04-22 22:08:55 -0300

    Promises

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-//consumir informações do servidor
+//promises -> códigos assíncronos (não depende da "linha do tempo" do resto do código)
 
-var xhr = new XMLHttpRequest();
+var minhaPromise = function () {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://api.github.com/users/guimorone");
+        xhr.send(null);
 
-xhr.open("GET", "https://api.github.com/users/guimorone");
-xhr.send(null);
-
-xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4) { //variável quando a resposta voltou pra gente
+                if(xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else{
+                    reject("Erro na requisição");
+                }
+            }
+        };
+    });
 };
+
+minhaPromise()
+    .then(response => {
+        console.log(response);
+    }) //executado quando chamar o resolve
+    .catch(error => {
+        console.log(error);
+    }); //executado quando chamar o reject
